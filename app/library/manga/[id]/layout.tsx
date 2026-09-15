d9efd896d1f34e614d68f05c79c2d01640304285
BuntaroMori90 <ciarlaflorindo90@gmail.com>
2026-09-15 08:37:42 +0200

    Guard manga collector on real catalog ids

--- app/library/manga/[id]/layout.tsx
+++ app/library/manga/[id]/layout.tsx
@@ -1,6 +1,9 @@
 import type { ReactNode } from "react";
 import { MangaCollectorPanel } from "./collector";
 
+const UUID =
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
 export default async function MangaDetailLayout({
   children,
   params,
@@ -13,7 +16,7 @@ export default async function MangaDetailLayout({
   return (
     <>
       {children}
-      <MangaCollectorPanel workId={id} />
+      {UUID.test(id) ? <MangaCollectorPanel workId={id} /> : null}
     </>
   );
 }
