7d59a10fe931cf3ecc605dbde588b454a5c6c451
BuntaroMori90 <ciarlaflorindo90@gmail.com>
2026-09-15 11:14:46 +0200

    Increase database query concurrency

--- lib/db.ts
+++ lib/db.ts
@@ -11,7 +11,7 @@ function createPool() {
 
   return new Pool({
     connectionString,
-    max: process.env.NODE_ENV === "production" ? 2 : 5,
+    max: process.env.NODE_ENV === "production" ? 4 : 5,
     idleTimeoutMillis: 30_000,
     connectionTimeoutMillis: 10_000,
     ssl: { rejectUnauthorized: false },
