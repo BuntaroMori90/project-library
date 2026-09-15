1cc24d28146e2757558784378d17af4e2c212ee2
BuntaroMori90 <ciarlaflorindo90@gmail.com>
2026-09-15 07:46:35 +0200

    Allow Kitsu manga cover images

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,8 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "static.tvmaze.com" },
       { protocol: "https", hostname: "cdn.myanimelist.net" },
       { protocol: "https", hostname: "api-cdn.myanimelist.net" },
+      { protocol: "https", hostname: "media.kitsu.app" },
+      { protocol: "https", hostname: "media.kitsu.io" },
     ],
   },
 };
